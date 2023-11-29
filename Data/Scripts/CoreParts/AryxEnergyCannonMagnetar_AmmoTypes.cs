a91b7b00e49455c89335b927c44855b96e7ddeef
savvygui <46466116+savvygui@users.noreply.github.com>
2023-11-29 14:42:19 -0600

    Update AryxEnergyCannonMagnetar_AmmoTypes.cs
    DesiredSpeed 1800 > 5500
    MaxTrajectory 5000 > 8000

--- Data/Scripts/CoreParts/AryxEnergyCannonMagnetar_AmmoTypes.cs
+++ Data/Scripts/CoreParts/AryxEnergyCannonMagnetar_AmmoTypes.cs
@@ -238,8 +238,8 @@ namespace Scripts
                 TargetLossTime = 0, // 0 is disabled, Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
                 MaxLifeTime = 0, // 0 is disabled, Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
                 AccelPerSec = 0f,
-                DesiredSpeed = 1800, // voxel phasing if you go above 5100
-                MaxTrajectory = 5000,
+                DesiredSpeed = 5500, // voxel phasing if you go above 5100
+                MaxTrajectory = 8000,
                 //FieldTime was here, it's dead now is disabled, a value causes the projectile to come to rest, spawn a field and remain for a time (Measured in game ticks, 60 = 1 second)
                 GravityMultiplier = 0f, // Gravity multiplier, influences the trajectory of the projectile, value greater than 0 to enable.
                 SpeedVariance = Random(start: 0, end: 0), // subtracts value from DesiredSpeed
